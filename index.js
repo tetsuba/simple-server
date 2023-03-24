08129b5db978de4dda70e30e4bd8bf7537b7928a
Tetsuba <tetsuba@tetsuba.com>
2023-03-24 10:10:05 +0000

    update

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}!`)
   console.log('http://localhost:3000')
 })
